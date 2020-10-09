60b098847c37032acfd57da291ac380a655f37fb
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-10-09 10:11:00 +0200

    Updates

---
+++ precache-manifest.bc17c352b0aa969317145692a9ea0fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c2b7fb19f45ef7dbe6862173aaaf1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a39413117bd5bb6173",
+    "url": "/static/css/main.b4ef154e.chunk.css"
+  },
+  {
+    "revision": "7a867d8ad95271194603",
+    "url": "/static/js/2.3cb8cf4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3cb8cf4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a39413117bd5bb6173",
+    "url": "/static/js/main.9059f385.chunk.js"
+  },
+  {
+    "revision": "1a53f7655c85a2e52874",
+    "url": "/static/js/runtime-main.adb27058.js"
+  }
+]);
