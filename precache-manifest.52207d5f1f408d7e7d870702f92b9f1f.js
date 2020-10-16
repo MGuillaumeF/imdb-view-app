e8fc289e451f9bafda6f726840d66155dab7d1ee
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-10-16 02:36:59 +0200

    Updates

---
+++ precache-manifest.52207d5f1f408d7e7d870702f92b9f1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94331e09e69d9da609090500d56d97d2",
+    "url": "/imdb-view-app/index.html"
+  },
+  {
+    "revision": "ff5a7a606c096e51fe76",
+    "url": "/imdb-view-app/static/css/main.e2fe31f9.chunk.css"
+  },
+  {
+    "revision": "977aa0cf9a2a41b9a84d",
+    "url": "/imdb-view-app/static/js/2.1b04cc3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imdb-view-app/static/js/2.1b04cc3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5a7a606c096e51fe76",
+    "url": "/imdb-view-app/static/js/main.99e26be0.chunk.js"
+  },
+  {
+    "revision": "c535c5607df20daabf44",
+    "url": "/imdb-view-app/static/js/runtime-main.84edaaba.js"
+  },
+  {
+    "revision": "3f6cf6792e4176ec56819764cafd979c",
+    "url": "/imdb-view-app/static/media/no_actor.3f6cf679.svg"
+  },
+  {
+    "revision": "80fa0699d170306cd39457b65ba6dd86",
+    "url": "/imdb-view-app/static/media/no_poster.80fa0699.svg"
+  }
+]);
