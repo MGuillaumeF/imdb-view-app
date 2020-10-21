fe70c4625c9c4bcb88f3886bb39eb8d5b721234b
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-10-22 01:58:56 +0200

    Ajout de la MenuBar

---
+++ src/components/MenuBar/MenuBar.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+import './MenuBar.sass'
+const menu = require('../../icons/menu.svg');
+function MenuBar(): ReactElement {
+    return (
+        <header className="MenuBar">
+            <img src={menu} alt="menu"/><h1>TITLE</h1>
+        </header>
+    )
+}
+
+export default MenuBar
