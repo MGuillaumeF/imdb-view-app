c42e485e9cbf4e301fbbe8c2fa35f84fb8cee829
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-10-22 02:30:30 +0200

    Updates

---
+++ docs/precache-manifest.456fb6ca6428d319cd0bf08cbf04b000.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088095b30aeee0b5b527",
+    "url": "build/2.088095b3.js"
+  },
+  {
+    "revision": "3f25926c337f6480faade6b8d709dee4",
+    "url": "build/2.088095b3.js.LICENSE.txt"
+  },
+  {
+    "revision": "87178402aa53bbdf3bb2",
+    "url": "build/bundle.87178402.js"
+  },
+  {
+    "revision": "2a0966e11623b821fca5",
+    "url": "build/main.2a0966e1.js"
+  },
+  {
+    "revision": "8952a50e5e53fb9a7edfbe8795778342",
+    "url": "index.html"
+  },
+  {
+    "revision": "2a0966e11623b821fca5",
+    "url": "static/css/main.1289ba75.chunk.css"
+  },
+  {
+    "revision": "baf95af3af5f201bc7538c1630c62c73",
+    "url": "static/media/heart.baf95af3.svg"
+  },
+  {
+    "revision": "31b1530d39c916d8a67c74ab6084f6c1",
+    "url": "static/media/menu.31b1530d.svg"
+  }
+]);
