fe1cff1047ad1f6c83f8abab31599223011ee6fa
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-10-27 01:24:09 +0100

    Updates

---
+++ docs/precache-manifest.2d2b95065762de93e99864a628cd3798.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a43ffaf3a2b1b86fbbe",
+    "url": "build/2.7a43ffaf.js"
+  },
+  {
+    "revision": "5da8278844b2b6bcebf94c67f84a4305",
+    "url": "build/2.7a43ffaf.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c9ee50ab8b78dc2e68",
+    "url": "build/bundle.d9c9ee50.js"
+  },
+  {
+    "revision": "59761b41b9fee870caf8",
+    "url": "build/main.59761b41.js"
+  },
+  {
+    "revision": "2a5086af40dcbcbda1746fc4b7b4cc3e",
+    "url": "index.html"
+  },
+  {
+    "revision": "59761b41b9fee870caf8",
+    "url": "static/css/main.1289ba75.chunk.css"
+  },
+  {
+    "revision": "1f19c69f8d1c668916e2c334df1d325d",
+    "url": "static/media/fr.1f19c69f.svg"
+  },
+  {
+    "revision": "baf95af3af5f201bc7538c1630c62c73",
+    "url": "static/media/heart.baf95af3.svg"
+  },
+  {
+    "revision": "85f634d44c93dc308dee0643488279c1",
+    "url": "static/media/menu.85f634d4.svg"
+  }
+]);
