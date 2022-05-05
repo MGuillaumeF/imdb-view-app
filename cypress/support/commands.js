4d94a5ef30cc758ddfda721119c973cfbb553a22
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-05-05 20:31:52 +0200

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,17 +24,17 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-const COMMAND_DELAY = 2000;
+const COMMAND_DELAY = 300;
 
 for (const command of [
-  'visit',
-  'click',
-  'trigger',
-  'type',
-  'clear',
-  'reload',
-  'contains',
-  'submit'
+  "visit",
+  "click",
+  "trigger",
+  "type",
+  "clear",
+  "reload",
+  "contains",
+  "submit"
 ]) {
   Cypress.Commands.overwrite(command, (originalFn, ...args) => {
     const origVal = originalFn(...args);
@@ -46,3 +46,25 @@ for (const command of [
     });
   });
 }
+
+Cypress.Commands.add("clearViewport", () => {
+  const runnerContainer =
+    window.parent.document.getElementsByClassName("iframes-container")[0];
+  runnerContainer.setAttribute(
+    "style",
+    "left: 0; top: 0; width: 100%; height: 100%;"
+  );
+
+  const sizeContainer =
+    window.parent.document.getElementsByClassName("size-container")[0];
+  sizeContainer.setAttribute("style", "");
+
+  const sidebar =
+    window.parent.document.getElementsByClassName("reporter-wrap")[0];
+  sidebar.setAttribute("style", "opacity: 0");
+
+  const header = window.parent.document.querySelector(
+    ".runner.container header"
+  );
+  header.setAttribute("style", "opacity: 0");
+});
